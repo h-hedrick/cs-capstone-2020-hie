e1b98a64a54beb088f54e3246e0823b774776abd
h-hedrick <48039915+h-hedrick@users.noreply.github.com>
2020-03-02 10:48:17 -0600

    working on making configs work, fixing import errors in authentication.service.ts, added webpack.config.js

--- hie-fred/src/app/_services/user.service.ts
+++ hie-fred/src/app/_services/user.service.ts
@@ -1,23 +1,25 @@
-from https://github.com/cornflourblue/angular-8-registration-login-example
+//from https://github.com/cornflourblue/angular-8-registration-login-example
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { User } from '@/_models';
+import { User } from '../_models';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
     constructor(private http: HttpClient) { }
 
-    getAll() {
-        return this.http.get<User[]>(`${config.apiUrl}/users`);
-    }
+    //we dont know if we need any of this!?
 
-    register(user: User) {
-        return this.http.post(`${config.apiUrl}/users/register`, user);
-    }
+    // getAll() {
+    //     return this.http.get<User[]>(`${config.apiUrl}/users`);
+    // }
 
-    delete(id: number) {
-        return this.http.delete(`${config.apiUrl}/users/${id}`);
-    }
+    // register(user: User) {
+    //     return this.http.post(`${config.apiUrl}/users/register`, user);
+    // }
+
+    // delete(id: number) {
+    //     return this.http.delete(`${config.apiUrl}/users/${id}`);
+    // }
 }
