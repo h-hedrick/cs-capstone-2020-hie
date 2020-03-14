f022d624ed5e80b2d0ea44ad5cec173771a69fb4
Darwin Johnson <27302097+DarwinJohnson@users.noreply.github.com>
2020-03-14 16:22:44 -0500

    Updated script_17_FA.py
    Ensured that the script was reading the correct file. Added a line to print the head of the table to preview. Commented the 17 variables that are needed to call createStudent from init_database.py and what is included in the csv file. Will need to restructure either the createStudent method or the csv files.

--- sql_scripts/csv_files/script_17_FA.py
+++ sql_scripts/csv_files/script_17_FA.py
@@ -2,6 +2,18 @@
 
 import pandas as pd 
 
-table = pd.read_csv("script_17_FA.py")
+# Read the CSV file as table, using the first row as the names for the table
+table = pd.read_csv("Sample Data 17_FA.csv", header=0)
 
+# Prints that no errors occured
 print("table read correctly")
+
+print(table.head())
+
+# For every row in the CSV, call createStudent from init_database.py
+
+# class, su_id, sex, pell_flag, first_gen_flag, first_race, second_race, 
+# hie_type, hie_name, hie_course_number, london_flag, dc_flag, city_name, country_name, hie_term, hie_year, 
+# fys_flag, fys_aes_term, fys_aes_year, graduated, grad_term, grad_year
+
+# Included in csv: su_id, sex, first_race, first_gen_flag, fys_flag, (transfer_flag), pell_flag
