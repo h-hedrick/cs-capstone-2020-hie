fc56d2df7bc22ec22721ebcc51db8cee915ac554
Matt <sanfordm@southwestern.edu>
2020-04-11 16:59:33 -0500

    Added CORS to flaskapp

--- hieapp/__init__.py
+++ hieapp/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 # from hieapp.dbcore import db
 
 #set up flask application
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
+CORS(app)
 
 # from hieapp.dbcore import db
